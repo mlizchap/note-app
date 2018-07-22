a372095acb8c32ef4767151265573a865ade1d7d
mlizchap <mlizchap@gmail.com>
2018-07-22 08:34:18 -0400

    NEW, used input instead of modifying elements

---
+++ src/components/Para/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+
+import Textarea from 'react-textarea-autosize';
+
+
+const textareaStyle = {
+    fontFamily: 'arial',
+    fontSize: '1.5em',
+    textAlign: 'center',
+    color: 'palevioletred',
+    border: 'none',
+    overflow: 'auto',
+    outline: 'none',
+    height: '30px',
+    width: '80%',
+    backgroundColor: 'orange',
+    resize: 'none'  
+}
+const ParaOutput = styled.p`
+    font-family: arial;
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+    min-height: 30px;
+    width: 80%;
+    background-color: orange;
+`;
+
+
+
+class Para extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { edit: false };
+    }
+    render() {
+        return ( 
+            <div>
+                { (this.props.edit) ? 
+                    <Textarea onChange={this.props.handleContent} onBlur={this.props.handleCreateOutput} defaultValue={this.props.content} style={textareaStyle} /> : 
+                    <ParaOutput onClick={this.props.handleEdit}>{this.props.content}</ParaOutput> 
+                }               
+            </div>
+        )
+   
+    }
+}
+
+export default Para;
--- src/components/note/index.js
+++ src/components/note/index.js
@@ -1,49 +1,115 @@
 import React, { Component } from 'react';
 import ContentEditable from 'react-contenteditable'
+import styled from 'styled-components'
+
+import EditableContent from '../EditableContent';
+import Para from '../Para';
+import './style.css';
 
 
 class Note extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            note: [
-                { type: 'h1', pos: 0, content: 'header', touched: false },
-                { type: 'p', pos: 1, content: 'paragraph', touched: false }
-            ],
-         };
+
+        this.state = {
+            editMode: true,
+            content: 'test'
+        }
     }
-    handleEdit = (e, content) => {
-        
+    changeEditMode = () => {
+        this.setState({ editMode: !this.state.editMode })
     }
-    handleChange = (e, content, index) => {
-        console.log(e.currentTarget.textContent)
-        const note = this.state.note;
-        note[index].content = e.currentTarget.textContent
-        this.setState({ note: note })        
+    updateContent = (e) => {
+        this.setState({ content: e.target.value})
+    }
+    handleEdit = (e) => {
+        console.log(e.target.value)
+    }
+    createOutput = (e) => {
+        var input = e.target.value
+        var start = 0;
+        for (var i = 0; i < input.length; i++) {
+            if (input[i] === "\n") {
+                input.slice(start, i);
+                start = i;
+            }
+        }
+        console.log(input.slice(start))
     }
     render() {
         return (
             <div>
-                <div className="buttons">
-                    <button onClick={this.addHeader}>New Header</button>
-                    <button>Add Code</button>
-                    <button>Highlight</button>
-                </div>
-                <div className="body">
-                    {this.state.note.map((note, index) => {
-                        return <ContentEditable 
-                                    key={index}
-                                    html={`<${note.type}>${note.content}</${note.type}>`}
-                                    disabled={false}
-                                    onChange={(e) => this.handleChange(e, note.content, index)}
-                                    onClick={(e) => this.handleEdit(e, note.index, note.content)}
-                                />        
-                    })}
-         
-                </div>
+                <Para 
+                    edit={this.state.editMode}
+                    content={this.state.content}
+                    handleEdit={this.changeEditMode}
+                    handleContent={this.updateContent}
+                    handleCreateOutput={this.createOutput}
+                />
             </div>
-        );
+        )
     }
+    
 }
+
+// class Note extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { 
+//             html: '<p>test</p>',
+//             note: [
+//                 { type: 'h1', content: 'header', html: '<h1>new header</h1><div>content here</div>', touched: false },
+//                 // { type: 'p', content: 'paragraph', touched: false }
+//             ],
+//             //currentParaValue
+//          };
+//     }
+//     // handleEdit = (e, content) => {
+//     //     touched = true;
+//     // }
+//     addHeader = () => {
+//         this.setState({ note: [...this.state.note, {type: 'h1', content: 'x', html: '<h1>new header</h1><div>content here</div>', touched: false} ]})
+//         //this.content.addEventListener('focus', console.log('x'))
+        
+//     }
+//     handleChange = (e, content, index, touched) => {
+//         const note = this.state.note;
+//         note[index].html=e.target.value;
+//         this.setState({note})
+
+//     }
+//     // handleEnterPress = (e) => {
+//     //     //console.log(this.content.htmlEl)
+//     //     if (e.key === 'Enter') {
+//     //         this.setState({ note: [...this.state.note, {type: 'p', content: 'x', touched: false} ]},  console.log(this.content.htmlEl))
+//     //     }
+//     // }
+//     render() {
+//         return (
+//             <div>
+//                 <div className="buttons">
+//                     <button onClick={this.addHeader}>New Header</button>
+//                     <button>Add Code</button>
+//                     <button>Highlight</button>
+//                 </div>
+//                 <div className="body">
+//                     {this.state.note.map((note, index) => {
+//                         return <ContentEditable 
+//                                     key={index}
+//                                     html={note.html}
+//                                     className="content"
+//                                     //html={`<${note.type}>${note.content}</${note.type}>`}
+//                                     disabled={false}
+//                                     //onKeyPress={this.handleEnterPress}
+//                                     onChange={(e) => this.handleChange(e, note.content, index, note.touched)}
+//                                     // onClick={(e) => this.handleEdit(e, note.index, note.content, note.touched)}
+//                                     ref={(input) => this.content = input}
+//                                 />        
+//                     })}
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
 
 export default Note;
